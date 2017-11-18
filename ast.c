244e0ce62f4670a9d9af371a8e6f3b20dfa60e21
Wilson Huang <wilsonhuang@Wilsons-MacBook-Pro.local>
2017-11-18 15:33:43 -0500

    see diffs

--- ast.c
+++ ast.c
@@ -115,6 +115,11 @@ void ast_free(node *ast) {
 
 void ast_print(node * ast) {
   printf("Hello world from the print function\n");
+  if (ast == NULL) {
+    //leaf node
+    //do nothing
+    return;
+  }
   node_kind kind = ast->kind; //important metadata
   //printing is pre-order: print self first
   print_node_type(ast);
@@ -126,42 +131,82 @@ void ast_print(node * ast) {
       ast_print(ast->scope.statements);
       break;
     case EXPRESSION_NODE:
+      //do nothing
       break;
     case UNARY_EXPRESION_NODE:
+      printf("UNARY_OP[%d]", ast->unary_expr.op)
+      ast_print(ast->unary_expr.right);
       break;
     case BINARY_EXPRESSION_NODE:
+      ast_print(ast->binary_expr.left);
+      printf("BINARY_OP[%d]\n", ast->binary_expr.op)
+      ast_print(ast->binary_expr.right);
       break;
     case INT_NODE:
+      printf("INT_NODE[%d]\n", ast->int_node.val);
       break;
     case FLOAT_NODE:
+      printf("INT_NODE[%f]\n", ast->float_node.val);
       break;
     case VAR_NODE:
+      printf("VAR_isArray[%d]\n", ast->var_node.is_array);
+      printf("VAR_array_index[%d]\n", ast->var_node.array_index);
+      printf("VAR_id: [%s]\n" ast->var_node.id);
       break;
     case FUNCTION_NODE:
+      printf("FUNCTION_NAME[%d]\n", ast->function_node.function);
+      ast_print(ast->function_node.arguments);
       break;
     case CONSTRUCTOR_NODE:
+      ast_print(ast->constructor_node.type);
+      ast_print(ast->constructor_node.arguments);
       break;
     case TYPE_NODE:
+      printf("TYPE[%d]\n", ast->type_node.token_type);
+      printf("TYPE_LENGTH[%d]\n", ast->type_node.token_type);
       break;
     case BOOL_NODE:
+      if (ast-bool_node.value != 0) {
+        printf("BOOL_NODE[TRUE]\n");
+      } else {
+        printf("BOOL_NODE[FALSE]\n");
+      }
       break;
     case STATEMENT_NODE:
+      ast_print(ast->statement_node.variable);
+      ast_print(ast->statement_node.expression);
       break;
     case STATEMENTS_NODE:
+      ast_print(ast->statements_node.statements);
+      ast_print(ast->statements_node.statement);
       break;
     case ARGUMENTS_NODE:
+      ast_print(ast->arguments_node.arguments);
+      ast_print(ast->arguments_node.expression);
       break;
     case EXPRESSION_VAR:
+      ast_print(ast->expression_var_node.var);
       break;
     case NESTED_EXPRESSION_NODE:
+      ast_print(ast->nested_expression_node.expression);
       break;
     case IF_STATEMENT_NODE:
+      ast_print(ast->if_statement_node.condition);
+      ast_print(ast->if_statement_node.bodyStatement);
+      ast_print(ast->if_statement_node.else_statement);
       break;
     case NESTED_SCOPE_NODE:
+      ast_print(ast->nested_scope_node.scope);
       break;
     case DECLARATIONS_NODE:
+      ast_print(ast->declarations_node.declarations);
+      ast_print(ast->declarations_node.declaration);
       break;
     case DECLARATION_NODE:
+      printf("DECLARATION_ISCONST[%d]\n", ast->declaration_node.is_const) //isconst
+      ast_print(ast->declaration_node.type);
+      printf("DECLARATION_NAME[%s]", ast->declaration_node.id) //id
+      ast_print(ast->declaration_node.expression);
       break;
     default: 
     break;
