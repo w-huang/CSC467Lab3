8f36d6419261b1f8588b986d370fd0c1c35f9f15
Wilson Huang <wilsonhuang@Wilsons-MacBook-Pro.local>
2017-11-19 00:00:03 -0500

    see diffs

--- compiler467.c
+++ compiler467.c
@@ -90,6 +90,7 @@ int main (int argc, char *argv[]) {
 /* Phase 3: Call the AST dumping routine if requested */
   //tag_scopes(ast);
   //build_symbol_table(ast);
+  printf("about to call dumpVariables");
   dumpVariables(ast);
 
   if (dumpAST)
